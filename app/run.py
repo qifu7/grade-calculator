c8a6398344296aea570e5777a9ef9a6d2c56d082
qifu7 <lookthisismine@gmail.com>
2023-09-12 06:31:54 +0800

    update

--- app/run.py
+++ app/run.py
@@ -29,3 +29,4 @@ optimistic_percentage_grade = GradeCalculator.calculate_optimistic_course_percen
 optimistic_letter_grade = GradeCalculator.calculate_letter_grade(optimistic_percentage_grade)
 print(f'If all other assignments are 100%, the overall course would be {optimistic_percentage_grade*100}%, which is a {optimistic_letter_grade}')
 
+###111111
